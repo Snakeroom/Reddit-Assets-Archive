5cdd598694647a88dacf875a1a9cf13d81960e4f
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 09:00:36 -0500

    Archive assets from 1/18/2020, 9:00:01 AM

--- 2020/Coins.js
+++ 2020/Coins.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/Coins.60299a96836113fb4290.js
-// Retrieved at 1/18/2020, 8:45:20 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 9:00:20 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Coins"], {
 		"./node_modules/lodash/times.js": function(e, a, t) {
