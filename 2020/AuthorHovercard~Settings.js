47588929d23c082fad307cbb47199bc3c101fa7b
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 11:00:48 -0500

    Archive assets from 1/18/2020, 11:00:01 AM

--- 2020/AuthorHovercard~Settings.js
+++ 2020/AuthorHovercard~Settings.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/AuthorHovercard~Settings.bcb3e30b7cc262c73863.js
-// Retrieved at 1/18/2020, 10:45:24 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 11:00:29 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["AuthorHovercard~Settings"], {
 		"./src/reddit/components/ChatButton/index.tsx": function(e, t, s) {
