99d28255c9649885d8c815053b0f21e9002c1fd0
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 04:45:39 -0500

    Archive assets from 1/18/2020, 4:45:01 AM

--- 2020/vendors~Governance~Reddit.js
+++ 2020/vendors~Governance~Reddit.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/vendors~Governance~Reddit.c0e808323891f3da58b8.js
-// Retrieved at 1/18/2020, 4:30:25 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 4:45:24 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["vendors~Governance~Reddit"], {
 		"./node_modules/@r/frames/compiled.js": function(e, n, o) {
