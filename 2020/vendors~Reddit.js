17a61c65c448f60b0262890662a384b91c0b8b71
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 10:00:38 -0500

    Archive assets from 1/18/2020, 10:00:01 AM

--- 2020/vendors~Reddit.js
+++ 2020/vendors~Reddit.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/vendors~Reddit.9c96a3a80dacda67c077.js
-// Retrieved at 1/18/2020, 9:45:22 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 10:00:23 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["vendors~Reddit"], {
 		"./node_modules/@r/google-tag-manager/compiled.js": function(e, t, o) {
