f18e8aae9cd70938dc46c74c55f4ae60d362aff5
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 03:30:37 -0500

    Archive assets from 1/18/2020, 3:30:01 AM

--- 2020/Frontpage.js
+++ 2020/Frontpage.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/Frontpage.d04b229d0dc113e04189.js
-// Retrieved at 1/18/2020, 3:15:20 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 3:30:21 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Frontpage", "PostDraft~RichTextEditor~StructuredStyles", "FrontpageSidebar"], {
 		"./node_modules/autosize/dist/autosize.js": function(e, t, s) {
