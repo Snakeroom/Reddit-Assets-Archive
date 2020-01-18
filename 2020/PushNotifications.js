d02cd9cdccf058bd2f90bf89535ff1b863dbe52f
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-17 21:45:33 -0500

    Archive assets from 1/17/2020, 9:45:01 PM

--- 2020/PushNotifications.js
+++ 2020/PushNotifications.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/PushNotifications.bcc727be3c5311eb87cf.js
-// Retrieved at 1/17/2020, 9:30:20 PM by Reddit Dataminer v1.0.0
+// Retrieved at 1/17/2020, 9:45:21 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["PushNotifications"], {
 		"./src/graphql/operations/RegisterWebPushToken.json": function(e) {
