8b2f4b684ab77b7c862d7b69da2e07cbaf0c4706
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 10:30:46 -0500

    Archive assets from 1/18/2020, 10:30:02 AM

--- 2020/SubredditHovercard.js
+++ 2020/SubredditHovercard.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/SubredditHovercard.a1eea320251c84666bd9.js
-// Retrieved at 1/18/2020, 10:15:23 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 10:30:27 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["SubredditHovercard"], {
 		"./src/reddit/components/CommunityType/index.m.less": function(e, t, s) {
