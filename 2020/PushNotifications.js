dbb095dce2eec7fe5bb57228d041ca54af603aa0
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-17 23:30:36 -0500

    Archive assets from 1/17/2020, 11:30:01 PM

--- 2020/PushNotifications.js
+++ 2020/PushNotifications.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/PushNotifications.bcc727be3c5311eb87cf.js
-// Retrieved at 1/17/2020, 11:15:22 PM by Reddit Dataminer v1.0.0
+// Retrieved at 1/17/2020, 11:30:23 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["PushNotifications"], {
 		"./src/graphql/operations/RegisterWebPushToken.json": function(e) {
--- 2020/vendors~EconHelperActions~Reddit.js
+++ 2020/vendors~EconHelperActions~Reddit.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/vendors~EconHelperActions~Reddit.e38ab53576589e9d9fdc.js
-// Retrieved at 1/17/2020, 11:15:22 PM by Reddit Dataminer v1.0.0
+// Retrieved at 1/17/2020, 11:30:23 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["vendors~EconHelperActions~Reddit"], {
 		"./node_modules/lodash/_LazyWrapper.js": function(o, e, s) {
