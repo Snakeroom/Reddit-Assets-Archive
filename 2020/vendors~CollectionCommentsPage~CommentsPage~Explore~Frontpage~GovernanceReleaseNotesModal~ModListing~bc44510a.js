8c4d1e4fea4fba22fda03c60b2e4ecfeddf66874
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-18 07:15:40 -0500

    Archive assets from 1/18/2020, 7:15:01 AM

--- 2020/vendors~CollectionCommentsPage~CommentsPage~Explore~Frontpage~GovernanceReleaseNotesModal~ModListing~bc44510a.js
+++ 2020/vendors~CollectionCommentsPage~CommentsPage~Explore~Frontpage~GovernanceReleaseNotesModal~ModListing~bc44510a.js
@@ -1,5 +1,5 @@
 // https://www.redditstatic.com/desktop2x/vendors~CollectionCommentsPage~CommentsPage~Explore~Frontpage~GovernanceReleaseNotesModal~ModListing~bc44510a.a7e4df1d25079ceac148.js
-// Retrieved at 1/18/2020, 7:00:24 AM by Reddit Dataminer v1.0.0
+// Retrieved at 1/18/2020, 7:15:23 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["vendors~CollectionCommentsPage~CommentsPage~Explore~Frontpage~GovernanceReleaseNotesModal~ModListing~bc44510a"], {
 		"./node_modules/bowser/src/bowser.js": function(e, t, o) {
