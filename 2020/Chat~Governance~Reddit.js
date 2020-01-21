053b2a4202af1a7c3f9900c9b491d055ff122188
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-21 15:50:18 -0500

    Archive assets from 1/21/2020, 3:50:01 PM

--- 2020/Chat~Governance~Reddit.js
+++ 2020/Chat~Governance~Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/Chat~Governance~Reddit.9e2fdd3111c966c0cc3d.js
-// Retrieved at 1/21/2020, 2:50:09 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/Chat~Governance~Reddit.384fdbf1a72738c46676.js
+// Retrieved at 1/21/2020, 3:50:10 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Chat~Governance~Reddit"], {
 		"./assets/fonts/IBMPlexSans/font.less": function(e, t, i) {},
@@ -12506,14 +12506,14 @@
 					}))
 				},
 				M = (e, t, i) => {
-					console.log("%cStarting Raven %crelease %c".concat("c8e7843-production") + " %cpublic url %c".concat(f.a.sentryClientPublicURL), "color: #7E53C1", "color: #7E53C1", "color: #FFB000", "color: #7E53C1", "color: #FFB000");
+					console.log("%cStarting Raven %crelease %c".concat("48dcdce-production") + " %cpublic url %c".concat(f.a.sentryClientPublicURL), "color: #7E53C1", "color: #7E53C1", "color: #FFB000", "color: #7E53C1", "color: #FFB000");
 					let n = [];
 					n = [new RegExp("^".concat(f.a.assetPath), "i")];
 					s.e({
 						attachStacktrace: !0,
 						dsn: f.a.sentryClientPublicURL,
 						whitelistUrls: n,
-						release: "c8e7843-production",
+						release: "48dcdce-production",
 						environment: "production",
 						ignoreErrors: ["$ is not defined"],
 						integrations: [...Object(C.d)(), new r.Integrations.Breadcrumbs({
@@ -13013,7 +13013,7 @@
 							settings: s,
 							statusCode: r,
 							type: o,
-							releaseClient: "c8e7843-production",
+							releaseClient: "48dcdce-production",
 							appName: t.statsAppName,
 							error: n ? JSON.parse(Object(a.a)(n)) : void 0
 						},
@@ -18662,4 +18662,4 @@
 		}
 	}
 ]);
-//# sourceMappingURL=Chat~Governance~Reddit.9e2fdd3111c966c0cc3d.js.map
+//# sourceMappingURL=Chat~Governance~Reddit.384fdbf1a72738c46676.js.map
--- 2020/Governance~Reddit.js
+++ 2020/Governance~Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/Governance~Reddit.eaa37d6e0f989b221a7a.js
-// Retrieved at 1/21/2020, 2:50:09 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/Governance~Reddit.620006c99f218a416fca.js
+// Retrieved at 1/21/2020, 3:50:10 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Governance~Reddit"], {
 		"./assets/fonts/NotoMono/font.less": function(e, t, n) {},
@@ -64458,4 +64458,4 @@
 		}
 	}
 ]);
-//# sourceMappingURL=Governance~Reddit.eaa37d6e0f989b221a7a.js.map
+//# sourceMappingURL=Governance~Reddit.620006c99f218a416fca.js.map
