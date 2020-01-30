0f6c49957b96baab8bc6a9abd69f7a75151d3432
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-01-30 15:20:28 -0500

    Archive assets from 1/30/2020, 3:20:01 PM

--- 2020/Governance~Reddit.js
+++ 2020/Governance~Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/Governance~Reddit.dc5b8f0f96c788d251ee.js
-// Retrieved at 1/30/2020, 3:10:16 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/Governance~Reddit.a05c1b7cfa1319d8b895.js
+// Retrieved at 1/30/2020, 3:20:14 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Governance~Reddit"], {
 		"./assets/fonts/NotoMono/font.less": function(e, t, n) {},
@@ -69578,4 +69578,4 @@
 		}
 	}
 ]);
-//# sourceMappingURL=Governance~Reddit.dc5b8f0f96c788d251ee.js.map
+//# sourceMappingURL=Governance~Reddit.a05c1b7cfa1319d8b895.js.map
