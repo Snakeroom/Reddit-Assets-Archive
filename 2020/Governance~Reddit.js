a5312d230d1b9efd0ce17998c1c60e22d0294bbb
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2020-06-24 19:50:39 -0400

    Archive assets from 6/24/2020, 7:50:01 PM

--- 2020/Governance~Reddit.js
+++ 2020/Governance~Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/Governance~Reddit.6d48c978e6d76a0380cb.js
-// Retrieved at 6/24/2020, 7:30:06 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/Governance~Reddit.7ce7d32dd070dd32a125.js
+// Retrieved at 6/24/2020, 7:50:32 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Governance~Reddit"], {
 		"./assets/fonts/NotoMono/font.less": function(e, t, n) {},
@@ -53841,4 +53841,4 @@
 		"ignored /drone/src/node_modules/clean-stack os": function(e, t) {}
 	}
 ]);
-//# sourceMappingURL=Governance~Reddit.6d48c978e6d76a0380cb.js.map
+//# sourceMappingURL=Governance~Reddit.7ce7d32dd070dd32a125.js.map
--- 2020/RpanListingUnit.js
+++ 2020/RpanListingUnit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/RpanListingUnit.df812311d1acda071321.js
-// Retrieved at 6/24/2020, 7:30:06 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/RpanListingUnit.720cadac47b761361aef.js
+// Retrieved at 6/24/2020, 7:50:32 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["RpanListingUnit"], {
 		"./node_modules/lodash/uniqueId.js": function(e, t, s) {
@@ -82,7 +82,7 @@
 			};
 			const x = (e => t => ((e, t) => "".concat(e, "(").concat((e => e.displayName || e.name || "Component")(t), ")"))(e, t))("WithMux"),
 				O = Object({
-					SENTRY_RELEASE_VERSION: "d77e6a0-production"
+					SENTRY_RELEASE_VERSION: "004fb18-production"
 				}),
 				T = {
 					anonymousUserId: "t2_anonymous",
@@ -1591,4 +1591,4 @@
 		}
 	}
 ]);
-//# sourceMappingURL=RpanListingUnit.df812311d1acda071321.js.map
+//# sourceMappingURL=RpanListingUnit.720cadac47b761361aef.js.map
