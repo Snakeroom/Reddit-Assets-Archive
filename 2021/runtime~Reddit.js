829be09a055ca7a406361d8200494d8f95286a23
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2021-10-07 23:20:07 +0200

    Archive assets from 10/07/2021, 5:20:01 PM

--- 2021/runtime~Reddit.js
+++ 2021/runtime~Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/runtime~Reddit.f71613a3d135bf2bcb5c.js
-// Retrieved at 10/7/2021, 5:00:05 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/runtime~Reddit.7cff2032d0183208d86d.js
+// Retrieved at 10/7/2021, 5:20:04 PM by Reddit Dataminer v1.0.0
 ! function(e) {
 	function o(o) {
 		for (var n, a, r = o[0], s = o[1], c = o[2], l = 0, b = []; l < r.length; l++) a = r[l], Object.prototype.hasOwnProperty.call(d, a) && d[a] && b.push(d[a][0]), d[a] = 0;
@@ -1401,7 +1401,7 @@
 						"translations/es_MX-json": "33da40e0ec6339320bc2",
 						"translations/fr_FR-json": "e7caa8dad0bffa05843a",
 						"translations/it_IT-json": "85d3271ae38aa13e53ad",
-						"translations/pseudo_ext-json": "bd9d8c05fb649e1e729a",
+						"translations/pseudo_ext-json": "5fd78551eee923703fb1",
 						"translations/pseudo_long-json": "3cb028dd96b72cfde4a6",
 						"translations/pseudo_short-json": "1f8f9c2f6e6a797abe43",
 						"translations/pt_BR-json": "547a79ba72b2cb659d13",
@@ -1662,4 +1662,4 @@
 	var m = c;
 	t()
 }([]);
-//# sourceMappingURL=https://www.redditstatic.com/desktop2x/runtime~Reddit.f71613a3d135bf2bcb5c.js.map
+//# sourceMappingURL=https://www.redditstatic.com/desktop2x/runtime~Reddit.7cff2032d0183208d86d.js.map
