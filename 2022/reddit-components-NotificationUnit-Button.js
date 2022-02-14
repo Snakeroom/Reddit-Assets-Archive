ef3bddaa8ede33f3072fe5c53b482b7eac195bdc
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2022-02-15 00:30:09 +0100

    Archive assets from 2/14/2022, 6:30:01 PM

--- 2022/reddit-components-NotificationUnit-Button.js
+++ 2022/reddit-components-NotificationUnit-Button.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/reddit-components-NotificationUnit-Button.d4e559585e16298f6f85.js
-// Retrieved at 2/14/2022, 6:20:04 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/reddit-components-NotificationUnit-Button.26ddb58b0bc8d5a84701.js
+// Retrieved at 2/14/2022, 6:30:06 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["reddit-components-NotificationUnit-Button"], {
 		"./src/reddit/actions/comment/authoring.ts": function(t, e, o) {
@@ -755,47 +755,44 @@
 				duration: l.a
 			};
 			e.default = t => {
-				var e, o, r, j, y, C, h, R, T, E;
-				const _ = Object(i.d)(),
+				var e, o, r, j, y, C, h, R, T;
+				const E = Object(i.d)(),
 					{
-						context: S,
-						deeplinkUrl: g,
-						isFullWidth: N,
-						isUnread: v,
-						isInboxCTAsAllEnabled: k,
-						isInboxCTAsReplyEnabled: A,
-						isInboxCTAsSeePostEnabled: D,
-						isNightMode: P,
-						notificationId: w,
-						onEvent: K
+						context: _,
+						deeplinkUrl: S,
+						isFullWidth: g,
+						isUnread: N,
+						isInboxCTAsAllEnabled: v,
+						isInboxCTAsReplyEnabled: k,
+						isInboxCTAsSeePostEnabled: A,
+						isNightMode: D,
+						notificationId: P,
+						onEvent: w
 					} = t,
-					M = null == S ? void 0 : S.messageType,
-					L = null === (e = null == S ? void 0 : S.post) || void 0 === e ? void 0 : e.permalink,
-					U = M === p.a.PostReply ? w : null === (o = null == S ? void 0 : S.comment) || void 0 === o ? void 0 : o.id,
-					F = (null === (r = null == S ? void 0 : S.post) || void 0 === r ? void 0 : r.id) || (null === (y = null === (j = null == S ? void 0 : S.comment) || void 0 === j ? void 0 : j.postInfo) || void 0 === y ? void 0 : y.id),
-					W = null === (C = null == S ? void 0 : S.subreddit) || void 0 === C ? void 0 : C.id,
-					B = null === (R = null === (h = null == S ? void 0 : S.awarding) || void 0 === h ? void 0 : h.award) || void 0 === R ? void 0 : R.id,
-					J = null === (E = null === (T = null == S ? void 0 : S.awarding) || void 0 === T ? void 0 : T.awarder) || void 0 === E ? void 0 : E.id;
+					K = null == _ ? void 0 : _.messageType,
+					M = null === (e = null == _ ? void 0 : _.post) || void 0 === e ? void 0 : e.permalink,
+					L = K === p.a.PostReply ? P : null === (o = null == _ ? void 0 : _.comment) || void 0 === o ? void 0 : o.id,
+					U = (null === (r = null == _ ? void 0 : _.post) || void 0 === r ? void 0 : r.id) || (null === (y = null === (j = null == _ ? void 0 : _.comment) || void 0 === j ? void 0 : j.postInfo) || void 0 === y ? void 0 : y.id),
+					F = null === (h = null === (C = null == _ ? void 0 : _.awarding) || void 0 === C ? void 0 : C.award) || void 0 === h ? void 0 : h.id,
+					W = null === (T = null === (R = null == _ ? void 0 : _.awarding) || void 0 === R ? void 0 : R.awarder) || void 0 === T ? void 0 : T.id;
 				return (() => {
-					switch (M) {
+					switch (K) {
 						case p.a.LifecyclePostSuggestions:
 						case p.a.SubredditRecommendation: {
-							if (!D && !k) return null;
+							if (!A && !v) return null;
 							const t = t => {
-								L ? K({
-									position: f.d.SECOND,
-									postId: F,
-									subredditId: W
-								}) : (t.preventDefault(), _(Object(l.f)(I)))
+								M ? w({
+									position: f.d.SECOND
+								}) : (t.preventDefault(), E(Object(l.f)(I)))
 							};
 							return s.a.createElement(u.t, {
 								onClick: t,
-								isFullWidth: N,
+								isFullWidth: g,
 								priority: u.c.Tertiary,
 								redditStyle: !0,
 								className: Object(c.a)(x.a.notificationButton, {
-									[x.a.isUnread]: v,
-									[x.a.isNightMode]: P
+									[x.a.isUnread]: N,
+									[x.a.isNightMode]: D
 								})
 							}, s.a.createElement(O.a, {
 								name: "text_post",
@@ -806,27 +803,27 @@
 						}
 						case p.a.PostReply:
 						case p.a.CommentReply: {
-							if (!A && !k) return null;
+							if (!k && !v) return null;
 							const t = t => {
-								if (F && U) {
-									const t = Object(d.a)(F, U, Object(b.c)(g)),
+								if (U && L) {
+									const t = Object(d.a)(U, L, Object(b.c)(S)),
 										e = {
-											parentCommentId: U,
+											parentCommentId: L,
 											commentsPageKey: t
 										};
-									_(Object(a.g)(e)), K({
+									E(Object(a.g)(e)), w({
 										position: f.d.SECOND
 									})
-								} else t.preventDefault(), _(Object(l.f)(I))
+								} else t.preventDefault(), E(Object(l.f)(I))
 							};
 							return s.a.createElement(u.t, {
 								onClick: t,
-								isFullWidth: N,
+								isFullWidth: g,
 								priority: u.c.Tertiary,
 								redditStyle: !0,
 								className: Object(c.a)(x.a.notificationButton, {
-									[x.a.isUnread]: v,
-									[x.a.isNightMode]: P
+									[x.a.isUnread]: N,
+									[x.a.isNightMode]: D
 								})
 							}, s.a.createElement(O.a, {
 								name: "reply",
@@ -836,24 +833,24 @@
 							}))
 						}
 						case p.a.AwardReceived: {
-							if (!J) return null;
+							if (!W) return null;
 							const t = t => {
-								t.preventDefault(), J ? K({
+								t.preventDefault(), W ? w({
 									position: f.d.SECOND,
-									awardId: B
-								}) : _(Object(l.f)(I))
+									awardId: F
+								}) : E(Object(l.f)(I))
 							};
 							return s.a.createElement(m.b, {
-								userId: J,
-								contextId: J,
+								userId: W,
+								contextId: W,
 								onClick: t,
-								isFullWidth: N,
+								isFullWidth: g,
 								priority: u.c.Tertiary,
 								redditStyle: !0,
 								eventSource: m.a.awardNotification,
 								className: Object(c.a)(x.a.notificationButton, {
-									[x.a.isUnread]: v,
-									[x.a.isNightMode]: P
+									[x.a.isUnread]: N,
+									[x.a.isNightMode]: D
 								})
 							}, s.a.createElement(O.a, {
 								name: "chat_new",
@@ -1090,4 +1087,4 @@
 		}
 	}
 ]);
-//# sourceMappingURL=https://www.redditstatic.com/desktop2x/reddit-components-NotificationUnit-Button.d4e559585e16298f6f85.js.map
+//# sourceMappingURL=https://www.redditstatic.com/desktop2x/reddit-components-NotificationUnit-Button.26ddb58b0bc8d5a84701.js.map
