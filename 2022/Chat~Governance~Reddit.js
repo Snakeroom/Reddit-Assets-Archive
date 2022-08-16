67f144578d4b59b7cbfe2adb0848a5690badf597
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2022-08-16 21:00:10 +0200

    Archive assets from 8/16/2022, 3:00:01 PM

--- 2022/Chat~Governance~Reddit.js
+++ 2022/Chat~Governance~Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/Chat~Governance~Reddit.84e981584a9b3a4790a4.js
-// Retrieved at 8/16/2022, 2:20:04 PM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/Chat~Governance~Reddit.267827533bb7fe9a4de9.js
+// Retrieved at 8/16/2022, 3:00:07 PM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Chat~Governance~Reddit"], {
 		"./assets/fonts/IBMPlexSans/font.less": function(e, t, i) {},
@@ -3538,11 +3538,11 @@
 				apiPassThroughHeaders: Object(r.e)({}.API_PASS_THROUGH_HEADERS || ""),
 				appName: {}.APP_NAME || "desktop2x",
 				assetPath: "https://www.redditstatic.com/desktop2x",
-				buildNumber: Object(r.c)("170821"),
+				buildNumber: Object(r.c)("170833"),
 				chatHelpUrl: {}.CHAT_HELP_URL || "https://reddit.zendesk.com/hc/en-us/sections/360008805652-Chat",
 				hlsVersion: "hls 0.12.4",
 				dashVersion: "dash 4.4.0",
-				buildTimestamp: Object(r.b)("1660671919"),
+				buildTimestamp: Object(r.b)("1660674239"),
 				cookieDomain: ".reddit.com",
 				giphyApiKey: "k2kwyMA6VeyHM6ZRT96OXDGaersnx73Z",
 				mediaUrl: "https://www.redditmedia.com",
@@ -6184,14 +6184,14 @@
 					}))
 				},
 				Y = (e, t, i) => {
-					console.log("%cStarting Raven %crelease %c944890992449ee4d9a2cf0f6e3183c9182898adb-production" + ` %cpublic url %c${y.a.sentryClientPublicURL}`, "color: #7E53C1", "color: #7E53C1", "color: #FFB000", "color: #7E53C1", "color: #FFB000");
+					console.log("%cStarting Raven %crelease %c37f653ff41b4ff184281f31a5e4b21197d81adc5-production" + ` %cpublic url %c${y.a.sentryClientPublicURL}`, "color: #7E53C1", "color: #7E53C1", "color: #FFB000", "color: #7E53C1", "color: #FFB000");
 					let n = [];
 					n = [new RegExp(`^${y.a.assetPath}`, "i")];
 					o.e({
 						attachStacktrace: !0,
 						dsn: y.a.sentryClientPublicURL,
 						whitelistUrls: n,
-						release: "944890992449ee4d9a2cf0f6e3183c9182898adb-production",
+						release: "37f653ff41b4ff184281f31a5e4b21197d81adc5-production",
 						environment: "production",
 						ignoreErrors: ["$ is not defined"],
 						integrations: [...Object(L.d)(), new d.Integrations.Breadcrumbs({
@@ -6727,7 +6727,7 @@
 							settings: r,
 							statusCode: s,
 							type: o,
-							releaseClient: "944890992449ee4d9a2cf0f6e3183c9182898adb-production",
+							releaseClient: "37f653ff41b4ff184281f31a5e4b21197d81adc5-production",
 							appName: t.statsAppName,
 							error: n ? JSON.parse(Object(l.a)(n)) : void 0
 						},
@@ -21322,7 +21322,7 @@
 				f = i("./src/reddit/selectors/subreddit.ts");
 			var b = i("./src/reddit/selectors/responsiveSettings.ts");
 			const g = new Set(["AntiEvilPlayground", "offmychest", "amitheasshole", "lgbt", "politics", "worldnews", "pics", "askreddit", "relationship_advice", "casualuk", "pokemongo", "de", "humansbeingbros", "liberalgunowners", "advice", "nostupidquestions", "europe", "bisexual", "askwomen", "ukpolitics", "france", "canada", "justnomil", "florida", "mildlyinteresting", "fortnitebr", "unexpected", "fantasy", "unitedkingdom", "nursing", "tooafraidtoask", "explainlikeimfive", "corgi", "starwarskenobi", "muaonthecheap", "nintendoswitch", "askhistorians", "kpop", "dccomics", "askfrance", "virginia", "science", "yurop", "changemyview", "equestrian"].map(e => e.toLowerCase())),
-				w = new Set(["antievilplayground", "lgbt", "dccomics", "yurop", "muaonthecheap", "loveislandusa", "blackladiesdating", "stpetersburgfl", "pics", "canada", "askuk", "fortnitebr", "unitedkingdom", "stopdrinking", "casualconversation", "animalsonreddit", "bisexual", "liberalgunowners", "redditsweats", "orangetheory", "fortnite", "readwithme", "dndgreentext", "ukraine", "auntienetwork", "amitheasshole", "tooafraidtoask", "crazyfuckingvideos", "politicalhumor", "abruptchaos", "canada", "videos", "maybemaybemaybe", "detroit", "doordash_drivers", "democrats", "florida", "usmc", "memphis", "onejob", "sonicthehedgehog", "lego", "veterans", "formuladank", "liberal", "worldnews", "shitposting", "whitepeopletwitter", "nfl", "polls", "indianteenagers", "science", "crappydesign", "inthenews", "columbus", "religion", "agedlikemilk", "ubereats", "israelpalestine", "orangecounty", "jerseyshore", "hamilton", "taylorswift", "askwomenover30", "hawaii", "hometheater", "rickandmorty", "piercing", "israel", "charlottehornets", "dynastyff", "instacartshoppers", "againsthatesubreddits", "asianamerican", "webdev", "prisonarchitect", "truegaming", "stopspeeding", "bestoflegaladvice", "idaho", "kdrama", "budgetaudiophile", "kdramarecommends", "canadahousing", "bangtan", "homedefense", "analog", "fantasy_football", "gifrecipes", "4ktv", "taskmaster", "windows", "southafrica", "windowshelp", "canadapublicservants", "belowdeck", "beautyguruchatter", "joebiden", "letterkenny", "health", "minecrafthelp", "lotr_on_prime", "halloween", "gymnastics", "juve", "justbootthings", "fortwayne", "chihuahua", "sisterwives", "windows10", "windows11", "nasa", "wilmingtonde", "philadelphiastars", "genderqueer", "bikela", "ladycyclists", "asiantwox", "transgender_surgeries", "web_design", "productmanagement", "startups", "40something", "crazyexstories", "learnmicrosoft", "learndotnet", "minimalspaces", "wildcats", "scritchthedamnbird", "redbubble", "goev", "toontown", "chia", "oled", "trophywiki", "facebookcringe", "facebookdrama"].map(e => e.toLowerCase())),
+				w = new Set(["antievilplayground", "lgbt", "dccomics", "yurop", "muaonthecheap", "loveislandusa", "blackladiesdating", "stpetersburgfl", "pics", "canada", "askuk", "fortnitebr", "unitedkingdom", "stopdrinking", "casualconversation", "animalsonreddit", "bisexual", "liberalgunowners", "redditsweats", "orangetheory", "fortnite", "readwithme", "dndgreentext", "ukraine", "auntienetwork", "amitheasshole", "tooafraidtoask", "crazyfuckingvideos", "politicalhumor", "abruptchaos", "canada", "videos", "maybemaybemaybe", "detroit", "doordash_drivers", "democrats", "florida", "usmc", "memphis", "onejob", "sonicthehedgehog", "lego", "veterans", "formuladank", "liberal", "worldnews", "shitposting", "whitepeopletwitter", "nfl", "polls", "indianteenagers", "science", "crappydesign", "inthenews", "columbus", "religion", "agedlikemilk", "ubereats", "israelpalestine", "orangecounty", "jerseyshore", "hamilton", "taylorswift", "askwomenover30", "hawaii", "hometheater", "rickandmorty", "piercing", "israel", "charlottehornets", "dynastyff", "instacartshoppers", "againsthatesubreddits", "asianamerican", "webdev", "prisonarchitect", "truegaming", "stopspeeding", "bestoflegaladvice", "idaho", "kdrama", "budgetaudiophile", "kdramarecommends", "canadahousing", "bangtan", "homedefense", "analog", "fantasy_football", "gifrecipes", "4ktv", "taskmaster", "windows", "southafrica", "windowshelp", "canadapublicservants", "belowdeck", "beautyguruchatter", "joebiden", "letterkenny", "health", "minecrafthelp", "lotr_on_prime", "halloween", "gymnastics", "juve", "justbootthings", "fortwayne", "chihuahua", "sisterwives", "windows10", "windows11", "nasa", "wilmingtonde", "philadelphiastars", "genderqueer", "bikela", "ladycyclists", "asiantwox", "transgender_surgeries", "web_design", "productmanagement", "startups", "40something", "crazyexstories", "learnmicrosoft", "learndotnet", "minimalspaces", "wildcats", "scritchthedamnbird", "redbubble", "goev", "toontown", "chia", "oled", "trophywiki", "facebookcringe", "facebookdrama", "fallout76marketplace"].map(e => e.toLowerCase())),
 				v = new Set(["uzitest", "testingtesting127", "kpkrunal"]);
 			var y = i("./src/reddit/featureFlags/subredditPoints.ts");
 			const E = new Set(["askaliberal", "asktransgender", "askwomen", "bisexual", "blackladies", "exchristian", "exmormon", "gendercynical", "girlgamers", "neoliberal", "nothowgirlswork", "progresspics", "subredditdrama", "teenagers", "thegirlsurvivalguide", "thequickbrowncorgi", "traaaaaaannnnnnnnnns", "transpositive", "trollxchromosomes", "twoxchromosomes"]);
@@ -49637,4 +49637,4 @@
 		"ignored /drone/src/node_modules/readable-stream/lib/internal/streams util": function(e, t) {}
 	}
 ]);
-//# sourceMappingURL=https://www.redditstatic.com/desktop2x/Chat~Governance~Reddit.84e981584a9b3a4790a4.js.map
+//# sourceMappingURL=https://www.redditstatic.com/desktop2x/Chat~Governance~Reddit.267827533bb7fe9a4de9.js.map
