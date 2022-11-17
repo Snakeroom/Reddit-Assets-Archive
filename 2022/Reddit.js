d5bc9c4372b9e3139ccf3a98b6732c93d3fa7516
Reddit-Assets-Archiver <60018743+Reddit-Assets-Archiver@users.noreply.github.com>
2022-11-17 11:30:10 -0500

    Archive assets from 11/17/2022, 11:30:01 AM

--- 2022/Reddit.js
+++ 2022/Reddit.js
@@ -1,5 +1,5 @@
-// https://www.redditstatic.com/desktop2x/Reddit.140f6c5a7df814dc8a11.js
-// Retrieved at 11/17/2022, 10:50:05 AM by Reddit Dataminer v1.0.0
+// https://www.redditstatic.com/desktop2x/Reddit.743db7903fa3cd7eb97e.js
+// Retrieved at 11/17/2022, 11:30:05 AM by Reddit Dataminer v1.0.0
 (window.__LOADABLE_LOADED_CHUNKS__ = window.__LOADABLE_LOADED_CHUNKS__ || []).push([
 	["Reddit"], {
 		"./assets/fonts/BentonSans/font.less": function(e, t, n) {},
@@ -17991,7 +17991,19 @@
 					const {
 						localStorageData: r
 					} = e;
-					r && t(eO(r))
+					r && t(eO(r)), (e => {
+						var t, n;
+						return !!(null === (n = null === (t = e.user) || void 0 === t ? void 0 : t.prefs) || void 0 === n ? void 0 : n.nightmode) !== window.matchMedia("(prefers-color-scheme: dark)").matches
+					})(s) && (e => {
+						!Object(O.R)(e) && Object(To.a)(e, {
+							rate: 1
+						}) && Object(B.a)({
+							...vn.o(e),
+							source: "global",
+							noun: "theme_os_mismatch",
+							action: "fail"
+						})
+					})(s)
 				}, nO = /^\/account\/magic_link\/landing\/[^\/]+\/?$/, sO = () => async (e, t) => {
 					var n;
 					(e => {
@@ -22045,4 +22057,4 @@
 		["./src/reddit/index.tsx", "runtime~Reddit", "vendors~CommentsPage~ModerationPages~Reddit~reddit-components-ClassicPost~reddit-components-CompactP~d737df3e", "vendors~CommentsPage~Governance~ModListing~ModerationPages~Reddit~Subreddit", "vendors~Chat~Governance~Reddit", "vendors~Reddit", "CommentsPage~Governance~Reddit~ReportFlow~Subreddit~reddit-components-BlankPost~reddit-components-Cl~5351df81", "Governance~Reddit~Subreddit~reddit-components-ClassicPost~reddit-components-CompactPost~reddit-compo~bd4baca2", "Reddit~RichTextEditor~reddit-components-LargePost~reddit-components-MediumPost~reddit-components-Not~05f7c62f", "Governance~ModListing~Reddit~ReportFlow~Subreddit", "Reddit~RpanListingUnit~StandalonePostPage~reddit-components-MediumPost", "Chat~Governance~Reddit", "Governance~Reddit~SubredditForkingCTA", "Governance~ModListing~Reddit", "Governance~Reddit", "ModListing~Reddit"]
 	]
 ]);
-//# sourceMappingURL=https://www.redditstatic.com/desktop2x/Reddit.140f6c5a7df814dc8a11.js.map
+//# sourceMappingURL=https://www.redditstatic.com/desktop2x/Reddit.743db7903fa3cd7eb97e.js.map
